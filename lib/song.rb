f0411dcfefcbe77f6778dd693d3277f43cf612a1
James Mobus <mobusj@live.com>
2019-09-10 19:05:11 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0;
+  @@artists = []
+  @@genres = []
+  
+  def initialize(name, artist, genre)
+    @@count += 1
+    
+    if !@@artists.contains(artist)
+      @@artists << artist 
+    end
+    
+    if !@@genres.contains(genre)
+      @@genres << genre 
+    end
+    
+  end
+  
+  def count
+    @@count
+  end
+  
+  def genres
+    @@genres 
+  end
+end
